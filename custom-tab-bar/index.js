72ea7a4fa090dce012ed1959cd4b886e55d5a2db
qdfudimo <1562124117@qq.com>
2021-12-28 16:33:44 +0800

    首次提交

---
+++ custom-tab-bar/index.js
@@ -0,0 +1,30 @@
+Component({
+  data: {
+    selected: 0,
+    color: "#7A7E83",
+    selectedColor: "#3cc51f",
+    list: [{
+      pagePath: "/pages/home/index",
+      iconPath: "/image/icon_component.png",
+      selectedIconPath: "/image/icon_component_HL.png",
+      text: "首页"
+    }, {
+      pagePath: "/pages/mine/index",
+      iconPath: "/image/icon_API.png",
+      selectedIconPath: "/image/icon_API_HL.png",
+      text: "我的"
+    }]
+  },
+  attached() {
+  },
+  methods: {
+    switchTab(e) {
+      const data = e.currentTarget.dataset
+      const url = data.path
+      wx.switchTab({url})
+      this.setData({
+        selected: data.index
+      })
+    }
+  }
+})
