e0846d1a9651131fd30d10167056be385c61cc13
qdfudimo <1562124117@qq.com>
2021-12-31 17:46:15 +0800

    下拉上拉加载

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,4 +1,6 @@
 // pages/home/index.js
+// const util = require('../../utils/util.js')
+// util.formatTime(new Date(log))
 Page({
 
   /**
@@ -16,7 +18,33 @@ Page({
     movePx: "0px",
     inputValue: "",
     showClose: false,
-    scheduleLsits:[]
+    scheduleLsits: [{
+      scheduleImg: "",
+      scheduleTime: "2/14日",
+      scheduleTheme: "新年安排",
+      likeCount: 6,
+      ifMyLike: 1,
+      scheduleLists: [{
+          id: 1,
+          time: "",
+          data: "新年开始了啊"
+        },
+        {
+          id: 2,
+          time: "",
+          data: "新年开始了啊"
+        },
+        {
+          id: 3,
+          time: "2021-11-22",
+          data: "新年开始了啊你在啥都hi打哈代发用于列表的索引分类显示和快速定位。货"
+        }
+      ],
+      userAvatr: "",
+      userName: "旺仔果冻"
+    }],
+    triggered: false,
+    arr: [1, 2, 3]
   },
   switchTab(e) {
     let {
@@ -37,6 +65,36 @@ Page({
     this.setData({
       inputValue: ""
     })
+  },
+  onRefresh(e) {
+    if (this._refreshing) return
+    this._refreshing = true;
+    setTimeout(() => {
+      this.setData({
+        triggered: false,
+        arr: [1, 2]
+      })
+      this._refreshing = false;
+    }, 3000)
+    console.log("onRefresh 自定义下拉刷新被触发", e);
+  },
+  onPulling(e) {
+    // bindrefresherpulling="onPulling"
+    console.log("onPulling 自定义下拉刷新控件被下拉", e);
+  },
+  loadMore(e) {
+    console.log("loadMore loadMore", e);
+    if (this._loadmoreIng) return
+    this._loadmoreIng = true;
+    setTimeout(() => {
+      this.setData({
+        arr: [1, 2, 3, 4, 5, 6]
+      })
+      this._loadmoreIng = false;
+    }, 2000)
+  },
+  onRestore(e) {
+    console.log("onRestore 自定义下拉刷新被复位", e);
   },
   /**
    * 生命周期函数--监听页面加载
--- utils/util.js
+++ utils/util.js
@@ -8,6 +8,12 @@ const formatTime = date => {
 
   return `${[year, month, day].map(formatNumber).join('/')} ${[hour, minute, second].map(formatNumber).join(':')}`
 }
+const handelTime = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  return `${[year, month, day].map(formatNumber).join('/')}`
+}
 
 const formatNumber = n => {
   n = n.toString()
@@ -15,5 +21,6 @@ const formatNumber = n => {
 }
 
 module.exports = {
-  formatTime
+  formatTime,
+  handelTime
 }
