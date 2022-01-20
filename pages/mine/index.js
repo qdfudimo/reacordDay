d48ed43b24fa509dec756ab070bd7b41e9434466
qdfudimo <1562124117@qq.com>
2022-01-20 17:41:59 +0800

    获取用户昵称、头像，分享功能

--- pages/mine/index.js
+++ pages/mine/index.js
@@ -11,7 +11,7 @@ Page({
     imgList: [],
     showBackground: false,
     currentBackground: "/image/background/rainbow.jpg",
-    isLogin: true,
+    hasUserInfo: false,
     ifCollect: false,
     defaultImg: "/image/background/rainbow.jpg"
   },
@@ -34,6 +34,20 @@ Page({
         console.error(res)
       }
     })
+    wx.getStorage({
+      key: "userInfo",
+      success: (res) => {
+        this.setData({
+          userInfo:JSON.parse(res.data),
+          hasUserInfo: true
+        })
+      },
+      fail: (error) => {
+        this.setData({
+          hasUserInfo: false
+        })
+      }
+    })
   },
   showIfBackground() {
     this.setData({
@@ -49,16 +63,68 @@ Page({
   selectBackground() {
 
   },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+        wx.setStorage({
+          key: "userInfo",
+          data: res.rawData
+        })
+      },
+      fail: (error) => {
+
+      }
+    })
+  },
   /**收藏小程序 */
   collectApplet() {
     this.setData({
       ifCollect: true
     })
-    setTimeout(()=>{
+    setTimeout(() => {
       this.setData({
         ifCollect: false
       })
-    },3000)
+    }, 3000)
+  },
+  setApplet() {
+    wx.showToast({
+      title: '暂未开发',
+      icon: 'error',
+      duration: 2000
+    })
+  },
+  shareApplet() {
+    // wx.showShareImageMenu({
+    //   path: "/image/sun.gif",
+    //   success() {},
+    //   fail() {}
+    // })
+
+  },
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage(item) {
+    // console.log(item);
+    const shareimg = [
+      "/image/write.png",
+      "/image/write1.png",
+      "/image/write2.png",
+      "/image/write3.png",
+    ]
+    const randomImg = shareimg[Math.floor(Math.random() * shareimg.length)];
+    return {
+      title: '写下你的生活',
+      path: '/page/mine/index',
+      imageUrl: randomImg,
+    }
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -105,11 +171,4 @@ Page({
   onReachBottom: function () {
 
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
